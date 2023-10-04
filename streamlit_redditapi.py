a7cd86f310c1c12c96bf7a928c3aaf0c0e90fa12
mysteriousbug <138992924+mysteriousbug@users.noreply.github.com>
2023-10-04 10:20:31 +0530

    Update streamlit_redditapi.py

--- streamlit_redditapi.py
+++ streamlit_redditapi.py
@@ -1,7 +1,33 @@
 import streamlit as st
-
+# Inject custom CSS with animation
+st.markdown(
+    f"""
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+        body {{
+            font-family: 'Roboto', sans-serif;
+            animation: pulse 5s infinite;
+            background-size: 100% 100%;
+            background-image: url('https://images.app.goo.gl/bG2QyLPmHfyBnfux5');  # Replace with your image URL
+            background-repeat: no-repeat;
+        }}
+        @keyframes pulse {{
+            0% {{
+                opacity: 0.7;
+            }}
+            50% {{
+                opacity: 0.5;
+            }}
+            100% {{
+                opacity: 0.7;
+            }}
+        }}
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 # Title
-st.title("Product Explanation Form")
+st.title("Reddit API")
 
 # Text box for product paragraph
 product_paragraph = st.text_area("Product Paragraph")
