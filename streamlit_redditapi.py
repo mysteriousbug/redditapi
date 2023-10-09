2c4963f7b291da816db549dd191d19508bb9e133
mysteriousbug <138992924+mysteriousbug@users.noreply.github.com>
2023-10-09 14:02:23 +0530

    Update streamlit_redditapi.py

--- streamlit_redditapi.py
+++ streamlit_redditapi.py
@@ -10,9 +10,6 @@ st.markdown(
         body {{
             font-family: 'Roboto', sans-serif;
             animation: pulse 5s infinite;
-            background-size: 100% 100%;
-            background-image: url('backgroundimg.png');  # Replace with your GitHub raw image URL
-            background-repeat: no-repeat;
         }}
         .custom-text-box {{
             height: 100px; /* Adjust the height as needed */
@@ -30,7 +27,7 @@ st.title("Reddit API")
 product_description = st.text_area("Product Description", key="product_desc")
 
 # Text box for subreddit URL(s)
-subreddit_urls = st.text_area("Subreddit URL(s)", key="subreddit_urls", height=100)
+subreddit_urls = st.text_area("Subreddit URL(s)", key="subreddit_urls")
 
 # Integer input for the number of posts to scrape per subreddit
 num_posts_to_scrape = st.number_input("Number of Posts to Scrape per Subreddit", min_value=1, step=1, key="num_posts")
@@ -44,14 +41,18 @@ if st.button("Run Code"):
     import time
     progress_bar = st.progress(0)
     for i in range(101):
-        time.sleep(0.1)
+        time.sleep(0.5)
         progress_bar.progress(i)
 
     st.text("Results Ready!")
 
-    # This will display any comments or outputs from your job
-    st.text("Comments:")
+    omments_data = [
+        ["Post Title 1", "Comment 1", "URL 1"],
+        ["Post Title 2", "Comment 2", "URL 2"]
+    ]
 
-    # Display comments inside boxes
-    for comment in ["Comment 1", "Comment 2"]:
-        st.text(comment)
+    for comment_info in comments_data:
+        post_title, comment, url = comment_info
+        st.write(f"**Post Title:** {post_title}")
+        st.write(f"**Comment:** {comment}")
+        st.write(f"**URL:** {url}")
