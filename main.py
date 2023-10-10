87ba165619bde64ea058b08c168d56a4f17e5275
mysteriousbug <138992924+mysteriousbug@users.noreply.github.com>
2023-10-10 17:01:46 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+def update_data(product_description, ignore_subreddits, time_cutoff_seconds, subreddits, postlimit):
+  data = {
+    "comment1": {
+        "comment_text": "This is the first comment",
+        "post": "Post 1",
+        "url": "URL 1"
+    },
+    "comment2": {
+        "comment_text": "This is the second comment",
+        "post": "Post 2",
+        "url": "URL 2"
+    },
+    "comment3": {
+        "comment_text": "This is the third comment",
+        "post": "Post 3",
+        "url": "URL 3"
+    }
+}
+
+return data
+
+
+
+
